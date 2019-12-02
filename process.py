cfe24d42c913c838ce8de17c893b128b0d33d99b
James Dunn <jhdunn@berkeley.edu>
2019-12-01 19:40:59 -0800

    Need to optimize for GPU.

--- process.py
+++ process.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import math
 import numpy as np
 import os
 from scipy import stats
@@ -8,8 +9,8 @@ import sys
 def main():
 
     project_name = sys.argv[1]
-    image_width = sys.argv[2]
-    scale_factor = sys.argv[3]
+    image_width = float(sys.argv[2])
+    scale_factor = float(sys.argv[3])
 
     points = np.loadtxt('unclassified_cloud.txt', usecols=range(4))
     offset = np.loadtxt('offset.txt', usecols=range(3))
@@ -20,7 +21,10 @@ def main():
     y_offset = offset[1]
     z_offset = offset[2]
 
-    for point in points:
+    for i in range(points.shape[0]):
+        point = points[i]
+        print('\n')
+        print(i)
         print('Now processing point:')
         print(point)
         pixel_classes = np.array([])
@@ -35,31 +39,31 @@ def main():
 
         prime_matrix = np.matrix([[x_prime], [y_prime], [z_prime], [1]])
 
-        for i in range(p_matrices.shape[0]):
-            filename = filenames[i]
+        for j in range(p_matrices.shape[0]):
+            filename = filenames[j]
+            try:
+                segmented_image = np.loadtxt(filename + '.txt', usecols=range(math.floor(image_width / scale_factor)))
+            except IOError as e:
+                continue
             print('Now processing filename:')
             print(filename)
-            p_matrix = np.matrix(p_matrices[i])
+            p_matrix = np.matrix(p_matrices[j])
             print('Now processing pmatrix:')
             print(p_matrix)
             p_matrix = np.reshape(p_matrix, (-1, 4))
             dot_matrix = np.matmul(p_matrix, prime_matrix)
             dot_matrix = np.reshape(dot_matrix, (-1, 3))
 
-            u = dot_matrix.A[0][0] / dot_matrix.A[0][2]
-            v = dot_matrix.A[0][1] / dot_matrix.A[0][2]
+            u = math.floor(dot_matrix.A[0][0] / dot_matrix.A[0][2])
+            v = math.floor(dot_matrix.A[0][1] / dot_matrix.A[0][2])
 
-            if u < 0 or v < 0:
+            if u < 0 or v < 0 or u >= math.floor(image_width / scale_factor) or v >= math.floor(image_width / scale_factor):
                 print ('Point is not found on this image')
                 continue
-
-            try:
-                segmented_image = np.loadtxt(filename + '.txt.', usecols=range(np.floor(image_width / scale_factor)))
-            except:
-                print ('Segmented image not found.')
-                continue
-
-            pixel_class = segmented_image.astype(int)[np.floor(u / scale_factor)][np.floor(v / scale_factor)]
+            else:
+                print('Point is found on this image.')
+             
+            pixel_class = segmented_image.astype(int)[math.floor(u / scale_factor)][math.floor(v / scale_factor)]
             pixel_classes = np.append(pixel_classes, pixel_class)
             print('Pixel classes now look like:')
             print(pixel_classes)
